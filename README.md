0b7c017f76736ad31b167788142c2e18773b21e5
Techwizz <paulrblack.prb@gmail.com>
2025-04-28 23:12:22 -0600

    Inital commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# dragNSort
+
+dragNSort is an awesome frontend library to add a draggable grid to your website.
+
+## Downloading
+
+You can include dragNSort with yarn:
+
+```bash
+yarn add dragnsort
+```
+
+You may also download a release from [GitHub](https://github.com/Techwizz-somboo/dragnsort/releases) or [dragnsort.dev](https://dragnsort.dev).
+
+## Usage
+
+Build a DOM structure like this:
+
+```html
+<div class="drag-list">
+    <div class="drag-item drag-w25">
+        <!-- Your elements here -->
+    </div>
+    <div class="drag-item drag-w25">
+        <!-- Your elements here -->
+    </div>
+</div>
+```
+
+The 2nd class, `drag-w25` is the size of the drag item, the `w` stands for width.
+
+Currently, there are 3 width options:
+* `drag-w25`
+* `drag-w33`
+* `drag-w50`
+
+The number at the end is the percentage of the grid you want the item to take. For example, `drag-w25` would allow for 4 items side by side before making a new row.
+
+If you need to initialize the list **after** the page has already been loaded, for example if you have dynamic content, you may call `initializeDragNSort();` in JS.
+
+In the event you need to listen for when a card is moved, there's the `handle-drag` event, which also returns the source and target nodes.
+
+Example usage:
+
+```js
+document.addEventListener('handle-drag', function (event) {
+    // Your code, for example maybe you need to redraw a chart
+    const sourceNode = event.detail.from;
+    const targetNode = event.detail.to;
+});
+```
+
+You may wish to change the circle drag image, here's an example on overriding it's style:
+
+```css
+.drag-icon {
+    color: var(--primary-color) !important;
+    position: absolute;
+    top: 12px !important;
+    left: 10px !important;
+    background-image: none !important;
+    font-family: 'themify';
+    font-size: 15px;
+    text-align: center;
+}
+
+.drag-icon::before {
+    content: "\e656"; /* Unicode for ti-move */
+}
+```
+
+If you have themify added to your project, this will use the `ti-move` icon.
+
+## Notes
+
+dragNSort **IS** mobile and tablet resposive! Grid item sizes automatically change when the screen size gets too small.
---
+++ dist/js/dragnsort.js
@@ -0,0 +1,144 @@
+/**
+* dragnsort version 1.0.0. Made by Paul Black. MIT license.
+* Visit https://dragnsort.dev/ for more information.
+* Source code: https://github.com/Techwizz-somboo/dragnsort/
+**/
+"use strict";
+
+var sourceNode = null;
+function DragNSort(config) {
+  this.$activeItem = null;
+  this.$container = config.container;
+  this.$items = this.$container.querySelectorAll('.' + config.itemClass);
+  this.dragStartClass = config.dragStartClass;
+  this.dragEnterClass = config.dragEnterClass;
+  this.parentDragClass = config.parentDragClass;
+}
+DragNSort.prototype.removeClasses = function () {
+  this.$items.forEach(function ($item) {
+    $item.classList.remove(this.dragStartClass, this.dragEnterClass);
+  }.bind(this));
+};
+DragNSort.prototype.isSameContainer = function (item1, item2) {
+  return item1 === item2;
+};
+DragNSort.prototype.on = function (elements, eventType, handler) {
+  elements.forEach(function (element) {
+    element.addEventListener(eventType, handler.bind(element, this), false);
+  }.bind(this));
+};
+DragNSort.prototype.onDragStart = function (_this, event) {
+  _this.$activeItem = this;
+  sourceNode = this;
+  this.classList.add(_this.dragStartClass);
+  event.dataTransfer.effectAllowed = 'move';
+  event.dataTransfer.setData('text/html', this.innerHTML);
+};
+DragNSort.prototype.onDragEnd = function (_this) {
+  this.classList.remove(_this.dragStartClass);
+};
+DragNSort.prototype.onDragEnter = function (_this) {
+  if (_this.isSameContainer(sourceNode.parentNode, _this.$container)) {
+    this.classList.add(_this.dragEnterClass);
+  }
+};
+DragNSort.prototype.onDragLeave = function (_this) {
+  if (_this.isSameContainer(sourceNode.parentNode, _this.$container)) {
+    this.classList.remove(_this.dragEnterClass);
+  }
+};
+DragNSort.prototype.onDragOver = function (_this, event) {
+  if (event.preventDefault) {
+    event.preventDefault();
+  }
+  event.dataTransfer.dropEffect = 'move';
+  return false;
+};
+DragNSort.prototype.onDrop = function (_this, event) {
+  if (event.stopPropagation) {
+    event.stopPropagation();
+  }
+  if (_this.$activeItem !== this && _this.isSameContainer(sourceNode.parentNode, _this.$container)) {
+    _this.$activeItem.innerHTML = this.innerHTML;
+    this.innerHTML = event.dataTransfer.getData('text/html');
+  }
+  var customEvent = new CustomEvent('handle-drag', {
+    detail: {
+      from: sourceNode,
+      to: this
+    }
+  });
+  document.dispatchEvent(customEvent);
+  DragNSort_adjustItemHeights();
+  _this.removeClasses();
+  sourceNode = null;
+  return false;
+};
+DragNSort.prototype.bind = function () {
+  this.on(this.$items, 'dragstart', this.onDragStart);
+  this.on(this.$items, 'dragend', this.onDragEnd);
+  this.on(this.$items, 'dragover', this.onDragOver);
+  this.on(this.$items, 'dragenter', this.onDragEnter);
+  this.on(this.$items, 'dragleave', this.onDragLeave);
+  this.on(this.$items, 'drop', this.onDrop);
+};
+DragNSort.prototype.setUniformHeight = function () {
+  var maxHeight = 0;
+  this.$items.forEach(function ($item) {
+    var itemHeight = $item.offsetHeight;
+    if (itemHeight > maxHeight) {
+      maxHeight = itemHeight;
+    }
+  });
+  this.$items.forEach(function ($item) {
+    $item.style.height = "".concat(maxHeight, "px");
+  });
+};
+DragNSort.prototype.init = function () {
+  var identifier = Math.random().toString(36).slice(2, 10);
+  this.$container.classList.add("drag-initialized-".concat(identifier));
+  this.$items.forEach(function ($item) {
+    var dragIcon = document.createElement('div');
+    dragIcon.classList.add('drag-icon');
+    dragIcon.setAttribute('draggable', true);
+    $item.appendChild(dragIcon);
+  });
+  this.setUniformHeight();
+  this.bind();
+};
+function initializeDragNSort() {
+  var listGroups = document.querySelectorAll('.drag-list:not([class*="drag-initialized"])');
+  listGroups.forEach(function (group) {
+    var draggable = new DragNSort({
+      container: group,
+      itemClass: 'drag-item',
+      dragStartClass: 'drag-start',
+      dragEnterClass: 'drag-enter'
+    });
+    draggable.init();
+  });
+  return true;
+}
+function DragNSort_adjustItemHeights() {
+  var listGroups = document.querySelectorAll('.drag-list');
+  listGroups.forEach(function (group) {
+    var items = group.querySelectorAll('.drag-item');
+    var maxHeight = 0;
+    items.forEach(function (item) {
+      item.style.height = 'auto';
+    });
+    items.forEach(function (item) {
+      maxHeight = Math.max(maxHeight, item.offsetHeight);
+    });
+    items.forEach(function (item) {
+      item.style.height = maxHeight + 'px';
+    });
+  });
+}
+document.addEventListener("DOMContentLoaded", function () {
+  DragNSort_adjustItemHeights();
+  window.addEventListener('resize', function () {
+    DragNSort_adjustItemHeights();
+  });
+  initializeDragNSort();
+});
